83832a38753b58101fe3da39919f48189da31316
Stefano Imperial Salvador <105392252+is-stefano@users.noreply.github.com>
2024-07-30 19:02:49 +0000

    Instagram terminado

--- src/api/models.py
+++ src/api/models.py
@@ -17,56 +17,71 @@ db = SQLAlchemy()
 #             "email": self.email,
 #             # do not serialize the password, its a security breach}
 
-class Users(db.model):
-    __tablename__ = "users"
-    id = db.column(db.integter, primary_key=True)
-    username = db.column(db.string(60), unique=True, nullable=False)
-    firstname = db.column(db.string(60), unique=True, nullable=False)
-    laststname = db.column(db.string(60), unique=True, nullable=False)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), unique=True, nullable=False)
+    firstname = db.Column(db.String(60), unique=True, nullable=False)
+    laststname = db.Column(db.String(60), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
          return f'<User {self.email} - {self.username}>'
 
-     def serialize(self):
+    def serialize(self):
         return {
-             "id": self.id,
-             'username': self.username,
-             'firstname': self.firstname,
-             'lastname': self.laststname,
-             "email": self.email }
+            "id": self.id,
+            'username': self.username,
+            'firstname': self.firstname,
+            'lastname': self.laststname,
+            "email": self.email }
 
     
-class Comments(db.model):
+class Comments(db.Model):
     __tablename__ = "comments"
-    id = db.column(db.integter, primary_key=True)
-    comment_text = db.column(db.string, nullable=False)
-    author_id = db.column(db.integer, db.ForeingKey['users_id'])
+    id = db.Column(db.Integer, primary_key=True)
+    comment_text = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author_to = db.relationship('Users', foreign_keys=[author_id])
-    post_id = db.column(db.integer, db.ForeingKey('posts.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
     post_to = db.relationship('Posts', foreign_keys=[post_id], backref=db.backref('comment_to', lazy='select'))
 
 
-class Posts(db.model):
+class Posts(db.Model):
     __tablename__ = "posts"
-    id = db.Column(db.integter, primary_key=True)
-    user_id = db.column(db.integer, db.ForeignKey('users.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('post_to', lazy='select'))
 
 
-class Followers(db.model):
+class Followers(db.Model):
     __tablename__ = "followers"
-    user_from_id = db.column(db.integer, db.ForeignKey('users.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    user_from_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user_from_to = db.relationship('Users', foreign_keys=[user_from_id])
-    user_to_id = db.column(db.intege, db.ForeignKey('users.id'))
-    user_to_to = db.relationship('Users' foreign_keys=[user_to_id])
+    user_to_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to_to = db.relationship('Users', foreign_keys=[user_to_id])
 
 
-class Medias(db.model):
+class Medias(db.Model):
     __tablename__ = "medias"
-    id = db.column(db.integter, primary_key=True)
-    comment_text = db.column(db.string, nullable=False)
-    author_id = db.column(db.integer)
-    post_id = db.column(db.integer)
+    id = db.Column(db.Integer, primary_key=True)
+    # media_type = db.Column(db.Enum('video', 'image', 'sounds', name="media_type") unique=True, nullable=False)
+    comment_text = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer)
+    post_id = db.Column(db.Integer)
+
+    def __repr__(self):
+            return f'<User {self.medias_type}'
+    
+    def serialize(self):
+         return{
+              'id': self.id,
+              'media_type': self.media_type,
+              'comment_text': self.comment_text,
+              'author_id': self.author_id,
+              'post_id': self.post_id
+         }
+    
 
 
